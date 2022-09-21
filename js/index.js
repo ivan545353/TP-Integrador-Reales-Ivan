48b68521bf712978696cda68491599fc41016847
Ivan Reales <ivan545353@gmail.com>
2022-09-21 13:20:02 -0300

    Inicio del Proyecto - Barra de Navegación

---
+++ js/index.js
@@ -0,0 +1,7 @@
+/* -------Script para el menu de hamburguesa--------*/
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click", () =>{
+    navMenu.classList.toggle("nav-menu_visible");
+});
